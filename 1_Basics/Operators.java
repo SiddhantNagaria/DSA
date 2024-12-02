1360bab70d3aff263392c30c1bb3ae75663b570c
Himanshi Kashyap <94634494+Himanshikashyap21@users.noreply.github.com>
2024-12-02 23:57:22 +0530

    Add files via upload

---
+++ 1_Basics/Operators.java
@@ -0,0 +1,88 @@
+public class Operators {
+    public static void main(String[] args) {
+
+        //Arithmetic Operators---->
+        // int A=20;
+        // int B=5;
+        // System.out.println("add=" +(A+B));
+        // System.out.println("subtract=" +(A-B));
+        // System.out.println("multiply=" +(A*B));
+        // System.out.println("divide=" +(A/B));
+        // System.out.println("modulo(remainder)=" +(A%B));
+
+
+
+        //Unary Operators----->
+        // int a=12;
+        // int b=++a;
+        // System.out.println(a);
+        // System.out.println(b);
+
+        // int a=10;
+        // int b=a++;
+        // System.out.println(a);
+        // System.out.println(b); 
+
+        // int a=10;
+        // int b=--a;
+        // System.out.println(a);
+        // System.out.println(b);
+
+        // int a=10;
+        // int b=a--;
+        // System.out.println(a);
+        // System.out.println(b);
+
+
+        //Relational Operators--->
+        // int A=10;
+        // int B=20;
+        // System.out.println(A==B);
+        // System.out.println(A!=B);
+        // System.out.println(A>B);
+        // System.out.println(A<B);
+        // System.out.println(A>=B);
+        // System.out.println(A<=B);
+
+
+        //Logical Operators---->
+        //AND
+        // System.out.println((3>2)&&(5>0));//true---only true true gives true in case of AND Operator
+        // System.out.println((3<2)&&(5>0));//false
+        // System.out.println((3>2)&&(5<0));//false
+        // System.out.println((3<2)&&(5<0));//false
+
+        // //OR
+        // System.out.println((3<2)||(5<0));//false---only false false gives false in case of OR Operator 
+        // System.out.println((3>2)||(5<0));//true
+        // System.out.println((3<2)||(5>0));//true
+        // System.out.println((3>2)||(5>0));//true
+
+        // //Logical NOT
+        // System.out.println(!(3>2));//false
+        // System.out.println(!(3<2));//true
+
+
+        //Assignment Operator
+        // int a = 10;
+        // a+=10;//a=a+10
+        // System.out.println(a);
+
+        // int b=5;
+        // b-=5; //b=b-5;
+        // System.out.println(b);
+
+        // int c=3;
+        // c*=5; //c=c*5
+        // System.out.println(c);
+
+        // int d=4;
+        // d/=2;//d=d/2;
+        // System.out.println(d);
+
+        // int e=6;
+        // e%=2;//e=e%2;
+        // System.out.println(e);
+    }
+    
+}
