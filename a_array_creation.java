1ccb19d234f215ea3b6ee561557621834541e0c8
Himanshi Kashyap <94634494+Himanshikashyap21@users.noreply.github.com>
2025-01-16 20:00:52 +0530

    Add files via upload

---
+++ a_array_creation.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+
+
+public class a_array_creation {
+    public static void main(String[] args) {
+        // Creating an array of integers......
+         int marks[] = new int[69];
+        // int numbers[]={1,2,3};
+
+
+        // Take Array as an input.......
+        // Scanner  sc=new Scanner(System.in);
+        // int physics_marks;
+        // physics_marks=sc.nextInt();
+
+        // marks[0]=sc.nextInt();
+        // marks[1]=sc.nextInt();
+        // marks[2]=sc.nextInt();
+        // System.out.println("maths : " + marks[0]);
+        // System.out.println("chemistry : " + marks[1]);
+        // System.out.println("physics : " + marks[2]);
+
+        // Update........
+        // marks[2]=100;
+        // System.out.println("physics :" + marks[2]);
+
+        // marks[2]=marks[2]+1;
+        // System.out.println("physics :" + marks[2]);
+
+        // Average.........
+        // int percentage=(marks[0]+marks[1]+marks[2])/3;
+        // System.out.println("Percentage : " + percentage +"%");
+
+        //Length Of Array....
+        System.out.println("Length Of Array = " + marks.length);
+
+    }
+    
+}
